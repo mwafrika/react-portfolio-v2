6ee867a85ee8f638022980f97d158b0d7d54a973
mwafrika <25848053+mwafrika@users.noreply.github.com>
2022-10-02 13:18:37 +0200

    Add dynamic data

---
+++ src/utils.js
@@ -0,0 +1,40 @@
+const info = [
+  {
+    id: 1,
+    title: 'TONIC',
+    speciality: ['CANOPY', 'Back End Dev', '2015'],
+    description:
+      'A daily selection of privately personalized reads; no accounts or sign-ups required.',
+    image: '/list/Snapshoot.svg',
+    skills: ['html', 'css', 'javascript'],
+  },
+  {
+    id: 2,
+    title: 'Multi-Post Stories',
+    speciality: ['FACEBOOK', 'Full Stack Dev', '2015'],
+    description:
+      'Experimental content creation feature that allows users to add to an existing story over the course of a day without spamming their friends.',
+    image: '/list/Snapshoot.svg',
+    skills: ['html', 'css', 'javascript'],
+  },
+  {
+    id: 1,
+    title: '',
+    speciality: ['CANOPY', 'Back End Dev', '2015'],
+    description:
+      'A daily selection of privately personalized reads; no accounts or sign-ups required.',
+    image: '/list/Snapshoot.svg',
+    skills: ['html', 'css', 'javascript'],
+  },
+  {
+    id: 1,
+    title: '',
+    speciality: ['CANOPY', 'Back End Dev', '2015'],
+    description:
+      'A daily selection of privately personalized reads; no accounts or sign-ups required.',
+    image: '/list/Snapshoot.svg',
+    skills: ['html', 'css', 'javascript'],
+  },
+];
+
+export default info;
